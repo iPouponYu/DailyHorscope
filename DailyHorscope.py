2995014670e82b5c438f59a4288a87c42c2be369
iPouponYu <ipouponyuu@gmail.com>
2021-10-08 02:05:44 +0200

    Added asking about what horscope user wants to know

--- DailyHorscope.py
+++ DailyHorscope.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+possible_horoscopes = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+
+print('Hello which horoscope would you like to know?')
+horoscope = input()
+for horoscopes in possible_horoscopes:
+    if horoscopes == horoscope:
+        print("Your daily horoscope for: " + horoscope + " is: ")
